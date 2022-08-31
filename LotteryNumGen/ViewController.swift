aafe2a0306b59112e98ddc22bc9707522c834011
AhmedAli-01 <axmed13@hotmail.com>
2022-08-31 19:58:32 +0100

    Initial Commit

---
+++ LotteryNumGen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LotteryNumGen
+//
+//  Created by Dr Toil on 31/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
